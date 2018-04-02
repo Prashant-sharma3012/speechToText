3bf076022fef0ca8ef43dd03b3e41a6d1e0e5c16
prashant-sharma3012 <sprashant.30@gmail.com>
2018-04-03 00:02:01 +0530

    server chnages

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require('path')
 
 var app = express();
 
-app.use(express.static('./client'));
+process.env.PWD = process.cwd();
+app.use(express.static(path.join(process.env.PWD, 'client')));
+
+// app.use(express.static('./client'));
 
 app.listen(8000,(err,data)=>{
     if(err) console.log(err);
